274aae2455a69c12ea16497c743f5a247c27f72d
Alejandro Paez <alepdlb@gmail.com>
2019-12-17 13:05:07 +0100

    Add winston logger and fix import settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import * as rateLimit from 'express-rate-limit';
-import * as helmet from 'helmet';
-import * as morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import { AppModule } from './app.module';
+import logger from './utils/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   app.use(morgan('dev'));
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
-  );
-  await app.listen(process.env.APP_PORT || 3000);
+  // app.use(
+  //   rateLimit({
+  //     windowMs: 15 * 60 * 1000, // 15 minutes
+  //     max: 100, // limit each IP to 100 requests per windowMs
+  //   }),
+  // );
+  const APP_PORT = process.env.APP_PORT || 3000;
+  const APP_HOST = process.env.APP_HOST || '0.0.0.0';
+  await app.listen(APP_PORT, APP_HOST, () => {
+    logger.info(`Server started at http://${APP_HOST}:${APP_PORT}`);
+  });
 }
 bootstrap();
---
+++ backend/src/utils/logger.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const tsFormat = new Date().toISOString();
+const logDir = process.env.LOGGING_DIR || 'logs';
+const logLevel = process.env.LOGGING_LEVEL || 'info';
+
+// Crear directorio de logs si no existe
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+/**
+ * Formato del log
+ */
+const myFormat = winston.format.printf(
+  ({ level, message, timestamp, stack }) => {
+    return `${timestamp} - ${level}: ${stack ? stack : message}`;
+  },
+);
+
+/**
+ * Crear nueva instancia de winston logger.
+ */
+const logger = winston.createLogger({
+  levels: winston.config.syslog.levels,
+  format: winston.format.combine(winston.format.errors({ stack: true })),
+  transports: [
+    new winston.transports.Console({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: tsFormat }),
+        myFormat,
+      ),
+    }),
+    new DailyRotateFile({
+      filename: `${logDir}/%DATE%-debug.log`,
+      datePattern: 'YYYY-MM-DD',
+      level: logLevel,
+      format: winston.format.combine(
+        winston.format.timestamp({ format: tsFormat }),
+        winston.format.json(),
+      ),
+    }),
+  ],
+});
+
+export default logger;
